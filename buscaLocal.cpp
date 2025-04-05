c76760a01ed38a417574626b1bfa678685e80bab
Vinicius Victor <viniciusvictor443@gmail.com>
2025-04-05 13:48:23 -0300

    Busca Local

--- buscaLocal.cpp
+++ buscaLocal.cpp
@@ -117,13 +117,13 @@ bool bestImprovement2opt(Solution &s)
 bool bestImprovementOrOpt(Solution &s, int bloco)
 {
     double best_delta = 0;
-    int best_insert, remove, valor;
+    int best_insert, remove, best_i, valor;
 
-    for (int i = 1; i < s.route.size() - 2; i++)
+    for (int i = 1; i < s.route.size() - bloco - 1; i++)
     {
         for (int j = 0; j < s.route.size() - 2; j++)
         {   
-            if (i - 1 <= j && j <= i) // se j == i não precisa calcular
+            if (i - 1 <= j && j <= i + bloco-1) 
                 continue;
 
             double delta = s.custoOrOpt(i, j, bloco);
@@ -131,8 +131,9 @@ bool bestImprovementOrOpt(Solution &s, int bloco)
             if (delta < best_delta)
             {
                 best_insert = j; // guarda qual melhor indice para adicionar
-                remove = (j < i) ? i + 1 : i; // indice a remover 
-                valor = s.route[i]; // guarda o valor a adicionar no insert
+                best_i = i;
+                remove = (j < i) ? i + bloco : i; // indice a remover 
+                valor = s.route[i];
                 best_delta = delta;
             }
         }
@@ -140,8 +141,10 @@ bool bestImprovementOrOpt(Solution &s, int bloco)
 
     if (best_delta < 0)
     {
-        s.route.insert(s.route.begin() + best_insert+1, valor);
-        s.route.erase(s.route.begin() + remove);
+        std::vector<int> copia(s.route.begin() + best_i, s.route.begin() + best_i + bloco);
+
+        s.route.insert(s.route.begin() + best_insert+1, copia.begin(), copia.end());
+        s.route.erase(s.route.begin() + remove, s.route.begin() + remove + bloco);
         s.cost += best_delta;
 
         return true;
@@ -152,5 +155,39 @@ bool bestImprovementOrOpt(Solution &s, int bloco)
 
 void BuscaLocal(Solution &s)
 {
-    bestImprovementOrOpt(s, 1);
+    std::vector<int> NL = {1, 2, 3, 4, 5};
+    int n;
+    bool improved = false;
+
+    while (!NL.empty())
+    {
+        n = rand() % NL.size(); 
+
+        switch (NL[n]){
+            case 1: 
+                improved = bestImprovementSwap(s);
+                break;
+            case 2: 
+                improved = bestImprovement2opt(s);
+                break;
+            case 3: 
+                improved = bestImprovementSwap(s);
+                break;
+            case 4: 
+                improved = bestImprovementOrOpt(s, 2);
+                break;
+            case 5: 
+                improved = bestImprovementOrOpt(s, 3);
+                break;
+        }
+
+        if (improved)
+        {
+            NL = {1, 2, 3, 4, 5};
+        }else
+        {
+            NL.erase(NL.begin() + n);
+        }
+    }
 }
+
