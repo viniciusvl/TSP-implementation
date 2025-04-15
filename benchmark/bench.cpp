5f264055fb6a1ac0b6e93a3d0e40520853d15f70
Vinicius Victor <viniciusvictor443@gmail.com>
2025-04-15 13:05:43 -0300

    bench

---
+++ benchmark/bench.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+int main(){
+    std::ifstream custos("custos.txt");
+    std::ifstream tempo("tempo.txt");
+    std::ofstream benchmark("benchmark.txt", std::ios::app);
+
+    double somaTempo = 0, somaCustos = 0;
+    double x = 0;
+    int count = 0;
+
+    while (tempo >> x)
+    {
+        somaTempo += x;
+        count++;
+    }
+
+    count = 0;
+    while (custos >> x)
+    {
+        somaCustos += x;
+        count++;
+    }
+
+    double mediaTempo = somaTempo / count;
+    double mediaCusto = somaCustos / count;
+
+    benchmark << "a280: " << " tempo: " << mediaTempo << " custo: " <<  mediaCusto << '\n';
+
+
+
+    return 0;
+}
