02d91d2485d456d6a358b7f935b8926efb73c77f
Vinicius Victor <viniciusvictor443@gmail.com>
2025-04-15 12:59:06 -0300

    testes

--- main.cpp
+++ main.cpp
@@ -6,22 +6,15 @@
 #include <iostream>
 #include <chrono>
 #include <fstream>
-#include <set>
-
-int temDuplicata(const std::vector<int>& vec) {
-    std::set<int> elementos(vec.begin(), vec.end());
-
-    return vec.size() - elementos.size();
-}
 
 int main(){
-    //std::ofstream custos("custos.txt", std::ios::app);
-    //std::ofstream tempo("tempo.txt", std::ios::app);
+    std::ofstream custos("custos.txt", std::ios::app);
+    std::ofstream tempo("tempo.txt", std::ios::app);
 
     char *argv[2];
 
     argv[0] = (char *) "TSP";
-    argv[1] = (char *) "instancias/eil101.tsp";
+    argv[1] = (char *) "instancias/burma14.tsp";
     srand(time(NULL));
 
     Data & data = Data::getInstance();
@@ -34,24 +27,18 @@ int main(){
     auto start = std::chrono::steady_clock::now(); 
     Solution s = Solve(maxIter, maxIterIls);
     auto end = std::chrono::steady_clock::now();
+
     s.print();
-
-    auto elapsed = std::chrono::duration<double>{end-start}.count();
-
 
     std::cout << "Custo corrigido: " << s.CustoRota() << '\n';
 
-    if (temDuplicata(s.route) > 1){
-        std::cout << "tem" << '\n';
-    }else{
-        std::cout << "nao tem" << '\n';
-    }
-    std::cout << "Tempo (s): " << elapsed << '\n';
-    //custos << s.cost << '\n';
-    //tempo << elapsed << '\n';
+    auto elapsed = std::chrono::duration<double>{end-start}.count();
+    std::cout << "Tempo: " << elapsed << '\n';
+    custos << s.cost << '\n';
+    tempo << elapsed << '\n';
 
-    //custos.close();
-    //tempo.close();
+    custos.close();
+    tempo.close();
 
 
     return 0;
