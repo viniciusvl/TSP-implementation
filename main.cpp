e064aa9521f86878a5e472b38c30f8e058ea5296
Vinicius Victor <viniciusvictor443@gmail.com>
2025-04-16 15:06:03 -0300

    finish tsp

--- main.cpp
+++ main.cpp
@@ -4,42 +4,22 @@
 #include "buscaLocal.h"
 #include <time.h>
 #include <iostream>
-#include <chrono>
-#include <fstream>
 
 int main(){
-    std::ofstream custos("custos.txt", std::ios::app);
-    std::ofstream tempo("tempo.txt", std::ios::app);
-
     char *argv[2];
 
     argv[0] = (char *) "TSP";
-    argv[1] = (char *) "instancias/burma14.tsp";
+    argv[1] = (char *) "instancias/CHOOSE_A_INSTANCE_HERE.tsp"; 
     srand(time(NULL));
 
     Data & data = Data::getInstance();
     data.read(2, argv); 
 
-    int maxIter, maxIterIls;
-    maxIterIls = (data.n >= 150) ? data.n / 2 : data.n;
-    maxIter = 50;
+    int maxIterIls = (data.n >= 150) ? data.n / 2 : data.n;
+    int maxIter = 50;
 
-    auto start = std::chrono::steady_clock::now(); 
     Solution s = Solve(maxIter, maxIterIls);
-    auto end = std::chrono::steady_clock::now();
-
     s.print();
-
-    std::cout << "Custo corrigido: " << s.CustoRota() << '\n';
-
-    auto elapsed = std::chrono::duration<double>{end-start}.count();
-    std::cout << "Tempo: " << elapsed << '\n';
-    custos << s.cost << '\n';
-    tempo << elapsed << '\n';
-
-    custos.close();
-    tempo.close();
-
 
     return 0;
 }
